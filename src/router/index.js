02ce95ec692db67666bdaf301e6689de49df076d
Tonny <musembitonnyj@gmail.com>
2022-01-07 10:34:29 +0300

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Tutorial from "../components/Tutorial.vue";
-const routes = [
-  {
-    path: "/",
-    name: "Tutorial",
-    component: Tutorial,
-  },
+
+const routes = [{
+        path: "/",
+        name: "Tutorial",
+        component: Tutorial,
+    },
+
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
